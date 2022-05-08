ad0729d0a64a9568e45e7700a701e1e76101c4c7
Adrian12377 <79296010+Adrian12377@users.noreply.github.com>
2022-05-07 22:06:09 -0700

    Cruceta
    Adrian Keith Duran

---
+++ src/components/Cuadros.js
@@ -0,0 +1,37 @@
+import "./Cuadros.css";
+export default function cuadro(){
+    return(
+        <>
+        <div className="caja16"></div>
+        <div className="caja7"></div>
+        <div className="caja8"></div>
+        <div className="caja9"></div>
+        <div className="caja10"></div>
+
+        <div className="caja17"></div>
+        <div className="caja19"></div>
+        <div className="caja18"></div>
+
+        <div className="caja20"></div>
+        <div className="caja21"></div>
+        <div className="caja23"></div>
+        <div className="caja22"></div>
+
+        <div className="caja24"></div>
+        <div className="caja25"></div>
+        <div className="caja2"></div>
+        <div className="caja1"></div>
+
+        <div className="caja3"></div>
+        <div className="caja4"></div>
+        <div className="caja6"></div>
+        <div className="caja5"></div>
+        <div className="caja11"></div>
+        <div className="caja12"></div>
+        <div className="caja13"></div>
+        <div className="caja14"></div>
+        <div className="caja15"></div>
+        
+        </>
+    );
+}
---
+++ src/components/Nombre.js
@@ -0,0 +1,16 @@
+export default function saludo(){
+    return(
+    <center>
+        <h1>
+        Adrian Keith Duran
+            <br>
+            </br>
+        Practica 2
+        <br>
+        </br>
+        Diseño de interfaces
+        </h1>
+    </center>
+        );
+
+}
